cae9db8cab358cf7dc246da326ab1215e7ff6e01
mernking <davtekdon@gmail.com>
2024-07-22 15:36:36 +0100

    mello update

--- src/app/api/blogs/[slug]/route.js
+++ src/app/api/blogs/[slug]/route.js
@@ -1,10 +1,10 @@
-import connectToDatabase from "../../../lib/mongoose";
-import Blog from "../../../models/blog";
+import connectToDatabase from "../../../../../lib/mongoose";
+import blog from "../../../../../models/blog";
 
 // Fetch all blog slugs
 async function fetchBlogSlugs() {
   await connectToDatabase();
-  const blogs = await Blog.find({}, "slug").exec();
+  const blogs = await blog.find({}, "slug").exec();
   return blogs.map((blog) => ({ slug: blog.slug }));
 }
 
@@ -19,19 +19,20 @@ export async function generateStaticParams() {
 }
 
 // Fetch the blog data for the specific slug
-export default async function handler(req, res) {
+export async function GET(req, res) {
   await connectToDatabase();
 
-  if (req.method !== 'GET') {
-    return res.status(405).json({ message: 'Method Not Allowed' });
+  if (req.method !== "GET") {
+    return res.status(405).json({ message: "Method Not Allowed" });
   }
 
-  const { slug } = req.query;
-  const blog = await Blog.findOne({ slug }).lean();
+  const slug = req.query; // Get the slug from the query parameters
+  console.log(slug);
+  const blogPost = await blog.findOne({ slug }).lean();
 
-  if (!blog) {
-    return res.status(404).json({ message: 'Blog not found' });
+  if (!blogPost) {
+    return Response.json({ message: "Blog not found" });
   }
 
-  return res.status(200).json(blog);
+  return Response.json(blogPost);
 }
