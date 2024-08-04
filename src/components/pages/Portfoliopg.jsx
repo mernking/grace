6588b80eac8d78a292a894e9e4a538f22177639b
mernking <davtekdon@gmail.com>
2024-08-05 00:52:31 +0100

    kings commit

---
+++ src/components/pages/Portfoliopg.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+export default function Portfoliopg() {
+  return (
+    <div className="w-full gap-2 flex flex-wrap justify-center">
+      <div className="portcard w-[250px] h-[350px] rounded-lg flex flex-col justify-center items-center bg-accent">
+        <p>man</p> 
+      </div>
+      <div className="portcard w-[250px] h-[350px] rounded-lg flex flex-col justify-center items-center bg-accent">
+        <p>man</p>
+      </div>
+      <div className="portcard w-[250px] h-[350px] rounded-lg flex flex-col justify-center items-center bg-accent">
+        <p>man</p>
+      </div>
+      <div className="portcard w-[250px] h-[350px] rounded-lg flex flex-col justify-center items-center bg-accent">
+        <p>man</p>
+      </div>
+      <div className="portcard w-[250px] h-[350px] rounded-lg flex flex-col justify-center items-center bg-accent">
+        <p>man</p>
+      </div>
+      <div className="portcard w-[250px] h-[350px] rounded-lg flex flex-col justify-center items-center bg-accent">
+        <p>man</p>
+      </div>
+      <div className="portcard w-[250px] h-[350px] rounded-lg flex flex-col justify-center items-center bg-accent">
+        <p>man</p>
+      </div>
+      <div className="portcard w-[250px] h-[350px] rounded-lg flex flex-col justify-center items-center bg-accent">
+        <p>man</p>
+      </div>
+    </div>
+  );
+}
