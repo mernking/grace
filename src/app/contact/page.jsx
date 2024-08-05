2ed0b46c7b93b92df1496da406dd60bd06fd2567
mernking <davtekdon@gmail.com>
2024-08-06 00:26:59 +0100

    mello woop

---
+++ src/app/contact/page.jsx
@@ -0,0 +1,10 @@
+import Contact from '@/components/pages/Contact';
+import React from 'react'
+
+export default function page() {
+  return (
+    <div className="contact">
+        <Contact />
+    </div>
+  );
+}
