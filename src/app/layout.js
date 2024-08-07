5a2ad88df772be44e034d3f580c2bdd512e1906b
mernking <davtekdon@gmail.com>
2024-08-07 23:50:08 +0100

    fixing

--- src/app/layout.js
+++ src/app/layout.js
@@ -24,6 +24,12 @@ export default function RootLayout({ children }) {
       lang="en"
       className={`dark bg-background text-[20px] font-futura flex flex-col justify-center items-center text-text ${futuraPTBook.variable} ${graceFont.variable}`}
     >
+      <head>
+        <meta
+          name="google-site-verification"
+          content="qtNIeFzTQ3iGZbUZ3RWoq-xqDhrn7bUogAdbhgeh7CM"
+        />
+      </head>
       <body className="max-w-[1080px] overflow-x-hidden">
         <div className="w-full flex flex-col justify-center items-center">
           <Navbar />
